e1be66ae4d0624d8b930d6e6756b6366c2b28fbb
Modman <101837874+windsbay@users.noreply.github.com>
2024-08-02 22:39:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host: 'mysql.b559fbfa7208.hosting.myjino.ru',
+    port: 3306,
+    user: 'j03809714_modman',
+    password: "aLeksey2011!",
+    database: 'j03809714_modmancomm'
+});
+
+conn.connect(err => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log('Connected to DATABASE');
+    }
+});
 
 const token = '7400849110:AAH4L-pCrJIHGhUBSHa2k4h6m0zzmQQ8rY8';
 
