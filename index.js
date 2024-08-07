29d36c80f801798ab704fb881f5698acb569ec3a
Modman <aleksey.modman@yandex.ru>
2024-08-07 22:38:10 +0300

    i

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ const bot = new TelegramBot(TOKEN, {polling: true});
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const  text = msg.text;
-
     if(text === "/start")
     {
         await bot.sendMessage(chatId, 'Received your message');
