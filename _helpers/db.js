f22f736f0ba4a1b0924439e7848963a7ce240b7f
Modman <aleksey.modman@yandex.ru>
2024-08-12 01:58:58 +0300

    i

--- _helpers/db.js
+++ _helpers/db.js
@@ -2,9 +2,9 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user: "postgres",
     password: "aLeksey2011",
-    host: "localhost",
+    host: "92.51.38.53",
     port: 5432,
-    database: "node_postgres"
+    database: "modman_comm"
 })
 
 
